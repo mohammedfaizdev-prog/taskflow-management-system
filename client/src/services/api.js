11b432a8973b7be4b5bad605840e65e4d1beda20
Mohammed Faiz <147734490+Mfaiz01@users.noreply.github.com>
2026-05-19 22:02:40 +0530

    Updated production API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+  "https://taskflow-management-system-production.up.railway.app/api",
 });
 
 export default API;
